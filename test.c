6c4043c37a35afcb28a5597e38ae22c781d15adc
ARTEMIS LAZANAKI <art.lazanaki@gmail.com>
2025-01-15 03:05:04 +0200

    3_1 almost done

--- test.c
+++ test.c
@@ -28,9 +28,9 @@ void simulate_generation(int *current, int *next, int start, int end, int cols, 
     for (int i = start; i < end; i++) {
         for (int j = 0; j < cols; j++) {
             int live_neighbors = 0;
-            int ii = (i - 1 + rows) % rows;
+            int ii = i -1;//(i - 1 + rows) % rows;
             int jj = (j - 1 + cols) % cols;
-            int iii = (i + 1 + rows) % rows;
+            int iii = i +1;//(i + 1 + rows) % rows;
             int jjj = (j + 1 + cols) % cols;
             live_neighbors = 
                 current[ii * cols + jj] +  // top left
@@ -75,58 +75,76 @@ int main(int argc, char *argv[]) {
     int local_rows = end_row - start_row;
     int *current = malloc((local_rows + 2) * grid_size * sizeof(int)); // +2 for ghost rows
     int *next = malloc((local_rows + 2) * grid_size * sizeof(int));
-
+    int grid[10][10]= {
+        {0, 0, 1, 0, 0, 1, 0, 0, 0, 1},
+        {0, 1, 1, 0, 1, 0, 0, 0, 0, 1},
+        {1, 0, 0, 0, 1, 0, 0, 0, 1, 1},
+        {1, 1, 1, 0, 0, 1, 0, 0, 1, 0},
+        {1, 1, 1, 1, 1, 0, 1, 0, 0, 1},
+        {1, 1, 0, 0, 1, 1, 0, 0, 0, 1},
+        {1, 1, 0, 0, 1, 0, 0, 1, 0, 1},
+        {1, 0, 1, 0, 1, 0, 1, 0, 0, 1},
+        {0, 0, 1, 0, 0, 0, 1, 0, 0, 1},
+        {1, 1, 0, 1, 0, 0, 0, 0, 1, 0}
+        };
     if (rank == 0) {
-        int *grid = malloc(grid_size * grid_size * sizeof(int));
+       // grid = malloc(grid_size * grid_size * sizeof(int));
         srand(time(NULL));
-        initialize_grid(grid, grid_size, grid_size);
-
-        // Print the initial grid for debugging
-        printf("Initial Grid (Process 0):\n");
+        //initialize_grid(grid, grid_size, grid_size);
+      
         print_grid(grid, grid_size, grid_size);
-
-        for (int p = 0; p < size; p++) {
-            int s_row = p * rows_per_process;
-            int e_row = s_row + rows_per_process;
-            if (p == 0) {
-                memcpy(current + grid_size, grid + s_row * grid_size, local_rows * grid_size * sizeof(int));
-            } else {
-                MPI_Send(grid + s_row * grid_size, (e_row - s_row) * grid_size, MPI_INT, p, 0, MPI_COMM_WORLD);
-            }
-        }
-        free(grid);
-    } else {
-        MPI_Recv(current + grid_size, local_rows * grid_size, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
+        // Print the initial grid for debugging
+       // printf("Initial Grid (Process 0):\n");
+      //  print_grid(grid, grid_size, grid_size);
 
-    // Print the received grid for debugging
-  //  printf("Process %d received grid:\n", rank);
-   // print_grid(current + grid_size, local_rows, grid_size);
-
-    // Print initial grid
-    // if (rank == 0) {
-    //     printf("Initial Grid:\n");
-    //     print_grid(current + grid_size, local_rows, grid_size);
-    // }
+       // Scatter the grid to all processes
+    MPI_Scatter(grid, local_rows * grid_size, MPI_INT, current + grid_size, local_rows * grid_size, MPI_INT, 0, MPI_COMM_WORLD);
+        
+    if (rank == 0) {
+    //    free(grid);
+    }
 
     for (int gen = 0; gen < generations; gen++) {
         MPI_Request send_request[2], recv_request[2];
         int num_requests = 0;
 
         if (rank > 0) {
+            // Send the first row of the local grid to the previous process
             MPI_Isend(current + grid_size, grid_size, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, &send_request[num_requests]);
-            MPI_Irecv(current, grid_size, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, &recv_request[num_requests]);
+            // Receive the ghost row from the previous process and put at the last ghost row
+            MPI_Irecv(current + (local_rows + 1) * grid_size, grid_size, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, &recv_request[num_requests]);
+            num_requests++;
+        } else {
+            // Send the first row of the local grid to the last process
+            MPI_Isend(current + grid_size, grid_size, MPI_INT, size - 1, 0, MPI_COMM_WORLD, &send_request[num_requests]);
+            // Receive the ghost row from the last process
+            MPI_Irecv(current + (local_rows+1)*grid_size, grid_size, MPI_INT, size - 1, 0, MPI_COMM_WORLD, &recv_request[num_requests]);
             num_requests++;
         }
 
         if (rank < size - 1) {
+            // Send the last row of the local grid to the next process
             MPI_Isend(current + local_rows * grid_size, grid_size, MPI_INT, rank + 1, 0, MPI_COMM_WORLD, &send_request[num_requests]);
-            MPI_Irecv(current + (local_rows + 1) * grid_size, grid_size, MPI_INT, rank + 1, 0, MPI_COMM_WORLD, &recv_request[num_requests]);
+            // Receive the ghost row from the next process
+            MPI_Irecv(current, grid_size, MPI_INT, rank + 1, 0, MPI_COMM_WORLD, &recv_request[num_requests]);
+            num_requests++;
+        } else {
+            // Send the last row of the local grid to the first process
+            MPI_Isend(current + local_rows * grid_size, grid_size, MPI_INT, 0, 0, MPI_COMM_WORLD, &send_request[num_requests]);
+            // Receive the ghost row from the first process
+            MPI_Irecv(current, grid_size, MPI_INT, 0, 0, MPI_COMM_WORLD, &recv_request[num_requests]);
             num_requests++;
         }
 
         MPI_Waitall(num_requests, send_request, MPI_STATUSES_IGNORE);
         MPI_Waitall(num_requests, recv_request, MPI_STATUSES_IGNORE);
+
+       
+        if(rank == 0 && gen == 0){
+            printf("rank 0 gen 0\n");
+            print_grid(current, local_rows + 2, grid_size);
+        }
 
         simulate_generation(current, next, 1, local_rows + 1, grid_size, grid_size);
 
@@ -143,8 +161,11 @@ int main(int argc, char *argv[]) {
                 int e_row = s_row + rows_per_process;
                 MPI_Recv(final_grid + s_row * grid_size, (e_row - s_row) * grid_size, MPI_INT, p, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             }
-          //  printf("Generation %d:\n", gen);
-           // print_grid(final_grid, grid_size, grid_size);
+            if(gen == 0||gen == 1 || gen == 2 || gen == 3 ){
+                print_grid(final_grid, grid_size, grid_size);
+            }
+         //  printf("Generation %d:\n", gen);
+         //  print_grid(final_grid, grid_size, grid_size);
             free(final_grid);
         } else {
             MPI_Send(current + grid_size, local_rows * grid_size, MPI_INT, 0, 0, MPI_COMM_WORLD);
