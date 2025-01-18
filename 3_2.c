fbdc9ab26c7a485c5a955cfd23c63a4d5a4d6e99
vicroshi <sdi1900096@di.uoa.gr>
2025-01-18 04:44:20 +0200

    3_2 finished?

---
+++ 3_2.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <time.h>
+#include <string.h>
+
+void initialize_matrix(double*, size_t, size_t);
+void initialize_vector(double*, size_t);
+void print_matrix(double*, size_t, size_t);
+void print_vector(double*, size_t, char*);
+
+int main(int argc, char* argv[]) {
+    double* A = NULL;
+    double* x = NULL;
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <n>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+    int rank, size;
+    size_t n = atoi(argv[1]);
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    int local_n = n / size;
+    double* local_a = malloc(local_n * n * sizeof(double));
+    // double* local_x = malloc(n * sizeof(double));
+    x = malloc(n * sizeof(double));
+    if (rank == 0) {
+        A = malloc(n * n * sizeof(double));
+        srand(17);
+        initialize_matrix(A, n, n);
+        initialize_vector(x, n);
+        print_vector(x, n, "x");
+        putchar('\n');
+        print_matrix(A, n, n);
+        MPI_Scatter(A, local_n*n, MPI_DOUBLE, local_a, local_n*n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        // MPI_Scatter(x, local_n, MPI_DOUBLE, local_x, local_n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Bcast(x, n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        free(A);
+        // free(x);
+    }
+    else{
+        MPI_Scatter(A, local_n*n, MPI_DOUBLE, local_a, local_n*n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        // MPI_Scatter(x, local_n, MPI_DOUBLE, local_x, local_n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        // print_vector(x, n, "x'");
+    }
+    MPI_Bcast(x, n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    // MPI_Allgather(local_x, local_n, MPI_DOUBLE, x, local_n, MPI_DOUBLE, MPI_COMM_WORLD);
+    // print_vector(x, n, "x'");
+    double* local_y = malloc(local_n * sizeof(double));
+    for (size_t i = 0; i < local_n; i++) {
+        local_y[i] = 0;
+        for (size_t j = 0; j < n; j++) {
+            local_y[i] += local_a[i * n + j] * x[j];
+        }
+    }
+
+    if(rank == 0){
+        double* y = malloc(n * sizeof(double));
+        MPI_Gather(local_y, local_n, MPI_DOUBLE, y, local_n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        print_vector(y, n, "y");
+        fflush(stdout);
+        free(y);
+    }
+    else{
+        MPI_Gather(local_y, local_n, MPI_DOUBLE, NULL, 0, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    }
+    free(local_a);
+    // free(local_x);
+    free(local_y);
+    free(x);
+    MPI_Finalize();
+    return 0;
+}
+
+void print_matrix(double* a, size_t rows, size_t cols) {
+    for (size_t i = 0; i < rows; i++) {
+        if(i==rows/2){
+            printf("A: ");
+        }
+        else{
+            printf("   ");
+        }
+        for (size_t j = 0; j < cols; j++) {
+            printf("%g ", a[i * cols + j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void print_vector(double* v, size_t n, char* name) {
+    printf("%s: [", name);
+    for (size_t i = 0; i < n; i++) {
+        printf("%g ", v[i]);
+    }
+    printf("\b]\n");
+}
+
+void initialize_matrix(double* a, size_t rows, size_t cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            a[i * cols + j] = rand() % 10;
+        }
+    }
+}
+
+void initialize_vector(double* a, size_t n) {
+    for (int i = 0; i < n; i++) {
+        a[i] = rand() % 10;
+    }
+}
